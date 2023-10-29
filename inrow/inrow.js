3107ad7318c62cbf0c321df160f306abf0f43f40
jubuenor <jubuenor@unal.edu.co>
2023-10-28 20:39:57 -0500

    non losing moves

--- inrow/inrow.js
+++ inrow/inrow.js
@@ -13,7 +13,7 @@ class Agent{
         this.node_count = 0;
         this.transposition_table = Array(this.size*this.size+1).fill({key:0, value:0});
         this.min_score=-(this.size*this.size)/2 + 3
-        this.max_score=(this.size*this.size+1)/2 - 3
+        this.max_score=(this.size*this.size+1)/2 - 3 
     }
 
     getIndex(key){
@@ -27,7 +27,6 @@ class Agent{
     }
 
     getTranspositionTable(pos){
-        //console.log(pos)
         if(pos >= this.transposition_table.length) return 0; 
         let i = this.getIndex(pos);
         if(this.transposition_table[i].key == pos)
@@ -36,14 +35,33 @@ class Agent{
             return 0;
     }
 
-    negamax(board, pos, alpha, beta , k){
-
-        if (alpha>=beta) return alpha;
-
-        this.node_count++;
-        if(this.board.check(board,pos)&&this.board.winner(board, pos) == this.color)
+    negamax(board, pos, alpha, beta){
+        
+        if(alpha>=beta) return alpha;
+        let boardClone = this.board.clone(board);
+        this.board.move(boardClone,pos,this.color);
+        if(this.board.winner(boardClone, this.k) == this.color)
             return (this.size*this.size+1 - this.moves)/2;
 
+
+        this.node_count++;
+
+        if(this.node_count>=10000) return alpha;
+
+        let nextMoves = this.getPossibleNonLosingMoves(board);
+
+        if(nextMoves.length == 0)
+            return -(this.size*this.size+1 - this.moves)/2;
+
+        if(this.moves >= this.size*this.size-2) return 0;
+
+        let min = -(this.size*this.size - 2 - this.moves)/2;
+
+        if(alpha<min){
+            alpha=min;
+            if (alpha>=beta) return alpha;
+        }
+        
         let max = (this.size*this.size - 1 - this.moves)/2;
        
         let val= this.getTranspositionTable(pos);
@@ -54,18 +72,21 @@ class Agent{
             if (alpha>=beta) return beta; 
         }
 
-        for(let x = 0; x<this.size; x++){
+        nextMoves.sort((a,b) => Math.abs(this.size/2 - a) - Math.abs(this.size/2 - b));
+
+        for(let i = 0; i<nextMoves.length; i++){
+            let x=nextMoves[i];
             if(this.board.check(board,x)){
                 let boardClone = this.board.clone(board);
                 this.board.move(boardClone,x,this.color);
-                let score = -this.negamax(boardClone, x , -beta, -alpha, k);
-                if (score>=beta) return beta;
+                let score = -this.negamax(boardClone, x , -beta, -alpha);
+                if (score>=beta) return score;
                 if (score>alpha) alpha=score;
             }
         }
         this.setTranspositionTable(pos, alpha - this.min_score + 1);
-        return alpha;
-    }
+        return alpha; 
+}
 
     getBestMove(scores){
         let max = -this.size*this.size/2;
@@ -98,48 +119,58 @@ class Agent{
     }
 
     solve(board,x){
+        let boardClone = this.board.clone(board);
+        this.board.move(boardClone,x,this.color);
+        if(this.board.winner(boardClone, this.k) == this.color)
+            return (this.size*this.size+1 - this.moves)/2;
+
         let min = -this.size*this.size/2;
         let max = this.size*this.size/2;
         while(min<max){
             let med = min + Math.floor((max-min)/2);
             if(med<=0&&min/2<med)med = min/2;
             else if(med>=0&&max/2>med)med = max/2;
-            let r = this.negamax(board,x,med,med+1,this.k);
+            let r = this.negamax(board,x,med,med+1);
             if (r<=med) max=r;
             else min=r;
         }
         return min
     }
 
+    getPossibleNonLosingMoves(board){
+        let possibleMoves = this.board.valid_moves(board);
+        let nonLosingMoves = [];
+        possibleMoves.map(x=>{
+            let boardClone = this.board.clone(board);
+            this.board.move(boardClone,x,this.color);
+            if(this.board.winner(boardClone, this.k) != this.color)
+                nonLosingMoves.push(x);
+        })
+        return nonLosingMoves;
+    }
+
     compute( board, time ){ 
 
+        let boardClone = this.board.clone(board);
         this.reset();
 
+        this.moves = board.reduce((a,b)=>a.concat(b)).filter(x=>x!=' ').length; 
 
-        this.moves = this.board.clone(board).filter(x => x != ' ').length; 
-
-        let possibleMoves = this.board.valid_moves(board);
+        console.log(this.moves)
+        let possibleMoves = this.board.valid_moves(boardClone);
+        
         possibleMoves.sort((a,b) => Math.abs(this.size/2 - a) - Math.abs(this.size/2 - b));
-        let scores = [];
+        let scores=[];
+        //console.log(scores)
 
-        for(let x = 0; x<possibleMoves.length; x++){
-            let boardClone = this.board.clone(board);
-            scores.push(this.solve(boardClone,possibleMoves[x]));
+        for(let x = 0; x<possibleMoves.length; x++){ 
+            let score = this.solve(boardClone,possibleMoves[x]);
+            scores.push(score);
         }
 
-        //console.log(this.transposition_table)
+        //console.log(this.color,possibleMoves,scores, possibleMoves[this.getBestMove(scores)]); 
 
-        //for(let i = 0; i<scores.length; i++){
-        //    console.log(possibleMoves[i],scores[i]);
-        //}
-
-        //let move = possibleMoves[this.getBestMove(scores)];
-        //console.log(move)
-
-        console.log(this.node_count);
-         
-        return possibleMoves[this.getBestMove(scores)]; 
-    
+        return possibleMoves[this.getBestMove(scores)];  
     }
 }
 
